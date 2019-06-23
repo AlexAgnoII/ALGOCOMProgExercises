f9317bf2c7c3a4e520f9606cad4a1689720e4d1c
AlexAgnoII <3alex23@gmail.com>
2019-06-23 21:56:21 +0800

    added time checker

--- src/Driver.java
+++ src/Driver.java
@@ -4,7 +4,18 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//Algorithm_Design_Writing.analyze("Alexander H. Agno II");
+		long start = System.nanoTime();
+		action();
+		long end = System.nanoTime();
+		
+		System.out.println("Time it took: " + ((double)(end - start) / 1000000000 ) + " seconds");
+		
+
+
+	}
+	
+	public static void action() {
+		Algorithm_Design_Writing.analyze("Alexander H. Agno II");
 		
 		//int[] arr = {1,2,3,4};
 		//Algorithm_Design_Writing.countSwaps(arr.length, arr);
@@ -12,8 +23,7 @@ public class Driver {
 //		int[] arr = {1000, 100, 100, 100, 100};
 //		Algorithm_Design_Writing.checkHarvest(arr.length, 200, arr);
 
-		Algorithm_Design_Writing.printCombination(9, 8);
-
+		//Algorithm_Design_Writing.printCombination(20, 3);
 	}
 
 }
