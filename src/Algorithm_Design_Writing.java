7496faab57356d0b5a0484206bfd1815d658a2f1
AlexAgnoII <3alex23@gmail.com>
2019-06-23 21:36:37 +0800

    Done (Double checking na lang)

--- src/Algorithm_Design_Writing.java
+++ src/Algorithm_Design_Writing.java
@@ -7,9 +7,15 @@ public class Algorithm_Design_Writing {
 	
 	private static final int N_MAX = 1000000;
 	private static final int M_MAX = 5000;
+	private static final String NOT_ENOUGH_FOOD = "NOT ENOUGH FOOD!";
+	private static final String JUST_ENOUGH_FOR_EVERYONE = "JUST ENOUGH FOR EVERYONE";
+	private static final String PARTY = "PARTY!";
 	
 	private static final char CHAR_MIN = 65;
 	private static final char CHAR_MAX = 90;
+	
+	
+	private static final int N_COMBINATION_MAX = 20;
 	
 	
 	public static void analyze(String input) {
@@ -79,7 +85,7 @@ public class Algorithm_Design_Writing {
 	
 	public static void countSwaps (int length, int[] carriageNumbers) {
 		   
-		int optimalCount = 0;
+		long optimalCount = 0;
 		
 		for(int x = 1; x < length; x++) {
 			
@@ -98,15 +104,68 @@ public class Algorithm_Design_Writing {
 		System.out.println("The most optimal train swaps: " + optimalCount);
 	}
 	
-	public static void checkHarvest (int numVillages, int costToFeed, int[] harvests) {
-		   //put your code here and the print statements for the output.
-	}
-
-	public static void printCombination (int N, int K) {
-		   //put your code here and the print statements for the output.
+	public static void checkHarvest (int numVillages, //N - number of villages to feed
+			                         int costToFeed, //M - cost to feed each villager.
+			                         int[] harvests) {
+		
+		long foodGoal = numVillages * costToFeed;
+		long totalHarvest = 0;
+		
+		if((numVillages >= 1 && numVillages <= N_MAX) &&
+		   (costToFeed >= 1 && costToFeed <= M_MAX)) {
+			
+			for(int i = 0; i < numVillages; i++) {
+				totalHarvest += harvests[i];
+			}
+			
+			if(totalHarvest < foodGoal) {
+				System.out.println(NOT_ENOUGH_FOOD);
+			}
+			
+			else if (totalHarvest > foodGoal) {
+				System.out.println(PARTY);
+			}
+			
+			else if (totalHarvest == foodGoal) {
+				System.out.println(JUST_ENOUGH_FOR_EVERYONE);
+			}
+		}
 	}
 	
-
+	public static void printCombination (int N, int K) { // nCk
+		
+		if((N >= 1 && N <= N_COMBINATION_MAX) &&
+			K >=1 && K <= N) {
+			int[] nElements = new int[N];
+			int[] temp = new int[N];
+			
+			for(int i = 0; i < N; i++) {
+				nElements[i] = i+1;
+			}
+			
+			doCombination(nElements, temp, 0, N, 0, K);
+		}
+		else {
+			System.out.println("Invalid N or K value.");
+		}
+	}
 	
-
+	private static void doCombination(int[] nElements, int[] temp, int start, int end, int index, int K) {
+		
+		//when the index is equal to the number of set K, print
+		if(index == K) {
+			for(int i = 0; i < K; i++) {
+				System.out.print(temp[i] + " ");
+			}
+			System.out.println();
+		}
+		
+		else {
+			//iterate over the tree of all possible combinations.
+			for(int i = start; i < end; i++) {
+				temp[index] = nElements[i];
+				doCombination(nElements, temp, i+1, end, index+1, K);
+			}
+		}
+	}
 }
--- src/Driver.java
+++ src/Driver.java
@@ -9,7 +9,11 @@ public class Driver {
 		//int[] arr = {1,2,3,4};
 		//Algorithm_Design_Writing.countSwaps(arr.length, arr);
 
-		
+//		int[] arr = {1000, 100, 100, 100, 100};
+//		Algorithm_Design_Writing.checkHarvest(arr.length, 200, arr);
+
+		Algorithm_Design_Writing.printCombination(9, 8);
+
 	}
 
 }
