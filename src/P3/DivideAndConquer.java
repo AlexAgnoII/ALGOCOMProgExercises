e31a530ac5bf9abab4b97838968cc31e00f701ad
AlexAgnoII <3alex23@gmail.com>
2019-07-17 21:39:54 +0800

    #1 done

--- src/P3/DivideAndConquer.java
+++ src/P3/DivideAndConquer.java
@@ -1,12 +1,111 @@
 package P3;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DivideAndConquer {
 	
 	public static void skyline (Building[] B) {
-		   //put your code here and the print statements for the output.
+		List<SkylinePoint> skylinePoints;
+		
+		skylinePoints = getSkylines(B, 0, B.length - 1);
+
+		for(SkylinePoint sp : skylinePoints) {
+			System.out.print(sp.x + " " + sp.y + " ");
+		}
+		System.out.println();
 	}
+	
+	private static List<SkylinePoint> getSkylines(Building[] B, int lhs, int rhs) {
+		
+		List<SkylinePoint> skyLinePointList = new ArrayList<SkylinePoint>();
+		if(lhs < rhs) {
+			int mid = (lhs + rhs) / 2;
+			List<SkylinePoint> leftSkylinePointList = getSkylines(B, lhs, mid);
+			List<SkylinePoint> rightSkylinePointList = getSkylines(B, mid + 1, rhs);
+			return mergeSkylines(leftSkylinePointList, rightSkylinePointList);
+		}
+		
+		//divide single building into 2 skyline point (1 building has 2 skyline point)
+		else if (lhs == rhs) {
+			SkylinePoint s1 = new SkylinePoint(B[lhs].left, B[lhs].height);
+			SkylinePoint s2 = new SkylinePoint(B[lhs].right, 0);
+			skyLinePointList.add(s1);
+			skyLinePointList.add(s2);
+		}
+		
+		return skyLinePointList;
+	}
+	
+	private static List<SkylinePoint> mergeSkylines(List<SkylinePoint> leftSkylinePointList, 
+			                                        List<SkylinePoint> rightSkylinePointList) {
+		
+		List<SkylinePoint> mergedSkylinePoints = new ArrayList<SkylinePoint>();
+		int leftHeight = 0; //
+		int rightHeight = 0;
+		int n = leftSkylinePointList.size();
+		int m = rightSkylinePointList.size();
+		
+		int i = 0; //index of left skyline points
+		int j = 0; //index of right skyline points 
+		
+		//left and right hand side of the list of skyline points are not empty.
+		while(i < n && j < m) {
+			SkylinePoint newSkylinePoint = new SkylinePoint();
+			
+			//compare the left x coordinate strip of a building between the leftskylinePoint and rightSkylinePoint
+			if(leftSkylinePointList.get(i).x < rightSkylinePointList.get(j).x) {
+				
+				newSkylinePoint.x = leftSkylinePointList.get(i).x;
+				leftHeight = leftSkylinePointList.get(i).y;		
+				newSkylinePoint.y = Math.max(leftHeight, rightHeight);
+				i++;
+			}
+			
+			else {
+				newSkylinePoint.x = rightSkylinePointList.get(j).x;
+				rightHeight = rightSkylinePointList.get(j).y;
+				newSkylinePoint.y = Math.max(leftHeight, rightHeight);
+				j++;
+			}
+			
+			addNewSkylinePoint(mergedSkylinePoints, newSkylinePoint);
+		}
+		
+		//add remaining strips that were not used.
+		while( i < n) {
+			mergedSkylinePoints.add(leftSkylinePointList.get(i));
+			i++;
+		}
+		
+		while(j < m ) {
+			mergedSkylinePoints.add(rightSkylinePointList.get(j));
+			j++;
+		}
+		
+		return mergedSkylinePoints;
+	}
+	
+	//check previous element and dont add unecessary skyline points (when new x or height is same as the recently added skyline point's x or height.)
+	private static void addNewSkylinePoint(List<SkylinePoint> mergedSkylinePoints, 
+			                        SkylinePoint newSkylinePoint) {
+		int n = mergedSkylinePoints.size();
+
+		
+		if(n > 0) {
+			if(mergedSkylinePoints.get(n - 1).y == newSkylinePoint.y)
+				return;
+			
+			else if(mergedSkylinePoints.get(n - 1).x == newSkylinePoint.x) {
+				mergedSkylinePoints.get(n - 1).y = Math.max(mergedSkylinePoints.get(n - 1).y, newSkylinePoint.y);
+				return;
+			}
+		}
+		//safetly add new skyline point.
+		mergedSkylinePoints.add(newSkylinePoint);
+	}
+	
+
 	
 	public static void mergesort (int[] A) {
 		int n = A.length;
@@ -21,8 +120,12 @@ public class DivideAndConquer {
         	    merge(A, lhs, rhs, mid);
         	}
         }
+        
+        for (int i = 0; i < n; i++) {
+        	System.out.print(A[i] + " ");
+        }
+        System.out.println();
 	}
-	
 	private static void merge(int[] A, int lhs, int rhs, int mid) {
 		
 		int n1Len = mid - lhs + 1;
@@ -63,15 +166,29 @@ public class DivideAndConquer {
 		   //put your code here and the print statements for the output.
 	}
 	
+	private static class SkylinePoint {
+		
+		public SkylinePoint(int left, int height) {
+			this.x = left;
+			this.y = height;
+		}
+		
+		public SkylinePoint() {
+			// TODO Auto-generated constructor stub
+		}
+
+		public int x;
+		public int y;
+		
+	}
 	
-	//Do we separate this to a different class for our testing or keep this here as is
-	public class Building {
+	public static class Building {
 		  public int left;  //the left X-coordinate
 		  public int right; //the right X-coordinate
 		  public int height; //the height
 	}
 	
-	public class Pt {
+	public static class Pt {
 		  public int x;  //X-coordinate
 		  public int y; // Y-coordinate
 	}
--- src/P3/P3_Driver.java
+++ src/P3/P3_Driver.java
@@ -1,8 +1,56 @@
 package P3;
 
+import P3.DivideAndConquer.Building;
+
 public class P3_Driver {
 
 	public static void main(String[] args) {
+		
+		Building[] B = new Building[6];
+		
+		B[0] = new Building();
+		B[0].left = 2;
+		B[0].height = 10;
+		B[0].right = 9;
+		
+		B[1] = new Building();
+		B[1].left = 3;
+		B[1].height = 15;
+		B[1].right = 6;
+		
+		B[2] = new Building();
+		B[2].left = 5;
+		B[2].height = 12;
+		B[2].right = 12;
+		
+		B[3] = new Building();
+		B[3].left = 13;
+		B[3].height = 10;
+		B[3].right = 16;
+		
+		B[4] = new Building();
+		B[4].left = 13;
+		B[4].height = 10;
+		B[4].right = 16;
+		
+		B[5] = new Building();
+		B[5].left = 15;
+		B[5].height = 5;
+		B[5].right = 17;
+		
+//		B[6] = new Building();
+//		B[6].left = 23;
+//		B[6].height = 13;
+//		B[6].right = 29;
+//		
+//		B[7] = new Building();
+//		B[7].left = 24;
+//		B[7].height = 4;
+//		B[7].right = 28;
+		
+		
+		
+		DivideAndConquer.skyline(B);
 
 	}
 
