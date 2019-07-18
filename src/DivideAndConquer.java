b9e0533584d5672b3477f421acd2d12a2fcebd7f
AlexAgnoII <3alex23@gmail.com>
2019-07-19 01:17:30 +0800

    Update DivideAndConquer.java

--- src/DivideAndConquer.java
+++ src/DivideAndConquer.java
@@ -323,6 +323,42 @@ public class DivideAndConquer {
 		  public int y; // Y-coordinate
 	}
 	
+	private static class Pair {
+		private Point p1;
+		private Point p2;
+		private double distance;
+		
+		public Pair(Point p1, Point p2, double distance) {
+			this.p1 = p1;
+			this.p2 = p2;
+			this.distance = distance;	
+		}
+
+		public Point getP1() {
+			return p1;
+		}
+
+		public void setP1(Point p1) {
+			this.p1 = p1;
+		}
+
+		public Point getP2() {
+			return p2;
+		}
+
+		public void setP2(Point p2) {
+			this.p2 = p2;
+		}
+
+		public double getDistance() {
+			return distance;
+		}
+
+		public void setDistance(double distance) {
+			this.distance = distance;
+		}
+	}
+	
 	private static class Point {
 		
 		private int x;
